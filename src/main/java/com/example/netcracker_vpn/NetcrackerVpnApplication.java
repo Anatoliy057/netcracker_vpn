6128d640b93d04a89a3a49da10759ddc3719a8cc
Udarczev Anatoliy <toly.udarczev@yandex.ru>
2020-11-22 14:27:16 +0300

    Init commit

---
+++ src/main/java/com/example/netcracker_vpn/NetcrackerVpnApplication.java
@@ -0,0 +1,13 @@
+package com.example.netcracker_vpn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetcrackerVpnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetcrackerVpnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/netcracker_vpn/NetcrackerVpnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.netcracker_vpn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetcrackerVpnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
